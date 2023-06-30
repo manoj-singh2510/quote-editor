ed18402c19c126aeec96eab24a6bacbe00b6d3f3
Manoj Solanki <manoj.solanki@gemsessence.com>
2023-06-30 21:40:42 +0530

    First commit.

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,50 @@
+class QuotesController < ApplicationController
+    before_action :set_quote, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @quotes = Quote.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @quote = Quote.new        
+    end
+
+    def create
+        @quote = Quote.new(quote_params)
+        if @quote.save
+            redirect_to quotes_path, notice: "Quote was suceessfully created"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @quote.update(quote_params)
+            redirect_to quotes_path, notice: "Quote was successfully update."
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @quote.destroy
+        redirect_to quote_path, notice: "Quote was suceessfully destroy."
+    end
+
+    private
+
+    def set_quote
+        @quote = Quote.find(params[:id])
+    end
+
+    def quote_params
+        params.require(:quote).permit(:name)
+    end
+end
